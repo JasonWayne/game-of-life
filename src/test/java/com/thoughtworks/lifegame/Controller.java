5304db3ea7e75f013b8e85bbe252fc708b5b05c6
jasonwayne <wuwenjie0102@gmail.com>
2017-06-03 10:24:27 +0800

    add javafx base

---
+++ src/test/java/com/thoughtworks/lifegame/Controller.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.lifegame;
+
+/**
+ * Created by wuwenjie on 6/3/17.
+ */
+public class Controller {
+
+}
---
+++ src/test/java/com/thoughtworks/lifegame/Planet.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.lifegame;
+
+/**
+ * Created by wuwenjie on 6/3/17.
+ */
+public class Planet {
+}
