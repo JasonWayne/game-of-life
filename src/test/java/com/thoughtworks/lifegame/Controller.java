2d46e81dc23eff6be19f05960f6ecc84297833c5
jasonwayne <wuwenjie0102@gmail.com>
2017-06-03 11:07:00 +0800

    finish test ui

--- src/test/java/com/thoughtworks/lifegame/Controller.java
+++ src/test/java/com/thoughtworks/lifegame/Controller.java
@@ -1,8 +1,50 @@
 package com.thoughtworks.lifegame;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 /**
  * Created by wuwenjie on 6/3/17.
  */
 public class Controller {
+
+    @FXML
+    Canvas canvas;
+
+    private GraphicsContext mGraphicsContext;
+
+    private Color colorChessBoard = Color.valueOf("#FBE39B");
+    private Color colorLine = Color.valueOf("#884B09");
+    private Color testColor = Color.valueOf("#888888");
+
+    private static final double BROAD_PADDING = 20;
+    private double gapX, gapY;
+
+    public void initialize() {
+        mGraphicsContext = canvas.getGraphicsContext2D();
+        gapX = (canvas.getWidth() - BROAD_PADDING * 2) / 20;
+        System.out.println(gapX);
+
+        gapY = (canvas.getWidth() - BROAD_PADDING * 2) / 20;
+
+        cleanPlanet();
+    }
+
+    private void cleanPlanet() {
+        mGraphicsContext.setFill(colorChessBoard);
+        mGraphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        mGraphicsContext.setStroke(colorLine);
+
+        mGraphicsContext.setFill(testColor);
+        mGraphicsContext.fillRect(BROAD_PADDING, BROAD_PADDING, gapX, gapX);
+
+        for (int i = 0; i <= 20; i++) {
+            mGraphicsContext.strokeLine(i * gapX + BROAD_PADDING, BROAD_PADDING, i * gapX + BROAD_PADDING, canvas.getHeight() - BROAD_PADDING);
+            mGraphicsContext.strokeLine(BROAD_PADDING, i * gapY + BROAD_PADDING, canvas.getWidth() - BROAD_PADDING, i * gapY + BROAD_PADDING);
+        }
+    }
 
 }
--- src/test/java/com/thoughtworks/lifegame/Main.java
+++ src/test/java/com/thoughtworks/lifegame/Main.java
@@ -1,18 +1,22 @@
 package com.thoughtworks.lifegame;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-public class Main extends Application {
+public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/lifegame.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Planet");
+        primaryStage.setScene(new Scene(root, 700, 700));
+        // TODO change to true
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -20,5 +24,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
 
